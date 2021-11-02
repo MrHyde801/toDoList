988bc99d82c6aa0abf87235a71af4e3ff6e312d7
Ian Hyde <mrhyde801@Ians-MacBook-Pro.local>
2021-11-02 14:04:00 -0600

    Adding functions to create and delete lists

--- main.js
+++ main.js
@@ -2,10 +2,30 @@
 const listsContainer = document.querySelector('[data-lists]');
 const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]')
+const deleteListButton = document.querySelector('[data-delete-list-button]')
+const listDisplayContainer = document.querySelector('[data-list-display-container]')
+const listTitleElement = document.querySelector('[data-list-title]')
+const listCountElement = document.querySelector('[data-list-count]')
+const tasksContainer = document.querySelector('[data-task]')
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = "task.selectedListId"
 
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+
+listsContainer.addEventListener('click', e => {
+    if(e.target.tagName.toLowerCase() === 'li') {
+        selectedListId = e.target.dataset.listId
+        saveAndRender()
+    }
+})
+
+deleteListButton.addEventListener('click', e => {
+    lists = lists.filter(list => list.id !== selectedListId)
+    selectedListId = null
+    saveAndRender()
+})
 
 newListForm.addEventListener('submit', e => {
     e.preventDefault()
@@ -23,6 +43,7 @@ function createList(name) {
 
 function save() {
     localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
 }
 
 function saveAndRender() {
@@ -32,11 +53,33 @@ function saveAndRender() {
 
 function render () {
     clearElement(listsContainer);
+    renderLists()
+
+    const selectedList = lists.find(list => list.id === selectedListId)
+    if(selectedListId == null) {
+    listDisplayContainer.style.display = 'none'
+    } else {
+    listDisplayContainer.style.display = ''
+    listTitleElement.innerText = selectedList.name
+    renderTaskCount(selectedList)
+    }
+}
+
+function renderTaskCount(selectedList) {
+    const incompleteTaskCount = selectedList.tasks.filter(task => !task.complete).length
+    const taskString = incompleteTaskCount === 1 ? "task" : "tasks"
+    lists
+}
+
+function renderLists() {
     lists.forEach(list => {
         const listElement = document.createElement('li');
         listElement.dataset.listId = list.id
         listElement.classList.add("list-name")
         listElement.innerText = list.name
+        if(list.id === selectedListId) {
+            listElement.classList.add('active-list')
+        }
         listsContainer.appendChild(listElement)
     })
 }
@@ -49,3 +92,11 @@ function clearElement(element) {
 
 render()
 
+
+
+
+
+//things to remove
+//change queory selectors to id selectors
+//probably get rid of the tasks remaining
+//add semicolon to everything
